1c91e275a7e7df8e3c417af1ec1d972fefce41c1
Oswin <129711775+OswinJTC@users.noreply.github.com>
2024-02-28 04:31:05 +0800

    Update TradeRepository.java

--- src/main/java/com/StockTracker/StockTracker/Trade/TradeRepository.java
+++ src/main/java/com/StockTracker/StockTracker/Trade/TradeRepository.java
@@ -4,6 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+ 
 @Repository
 public interface TradeRepository extends MongoRepository<Trade, ObjectId>{
+
+    Optional<Trade> findByReferenceNumber(int referenceNumber);
+
+
 }
