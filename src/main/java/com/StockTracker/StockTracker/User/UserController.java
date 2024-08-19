dcf686355e2004109ac1d602ee98c573911cb7e5
Oswin <129711775+OswinJTC@users.noreply.github.com>
2024-08-20 00:21:43 +0800

    add origin to https://stsa-member-client.onrender.com/

--- src/main/java/com/StockTracker/StockTracker/User/UserController.java
+++ src/main/java/com/StockTracker/StockTracker/User/UserController.java
@@ -18,7 +18,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://stsa-member-client.onrender.com/")
 @RequestMapping("/userApi")
 public class UserController {
 
